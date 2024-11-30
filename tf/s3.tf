32c70592932ae64296c2c057438c5a8e5a630f19
Inereaa <nerearojaas@gmail.com>
2024-11-30 21:25:37 +0100

    s3.tf corregido

--- tf/s3.tf
+++ tf/s3.tf
@@ -2,7 +2,6 @@
 # Crear un Bucket en S3
 resource "aws_s3_bucket" "mi_bucket" {
   bucket = "nmr-bucket"
-  force_destroy = true
 
   tags = {
     Name        = "nmr-bucket"
@@ -10,19 +9,21 @@ resource "aws_s3_bucket" "mi_bucket" {
   }
 }
 
+# Configurar el acceso público para el Bucket (si se desea)
 resource "aws_s3_bucket_public_access_block" "bucket_public_block" {
-  bucket = aws_s3_bucket.s3.id
+  bucket = aws_s3_bucket.mi_bucket.id
 
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
 
-  depends_on = [aws_s3_bucket.s3]
+  depends_on = [aws_s3_bucket.mi_bucket]
 }
 
+# Aplicar la política de acceso pública al Bucket
 resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.s3.id
+  bucket = aws_s3_bucket.mi_bucket.id
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -35,7 +36,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
           "s3:GetObject",
           "s3:PutObject"
         ]
-        Resource = "${aws_s3_bucket.s3.arn}/*"
+        Resource = "${aws_s3_bucket.mi_bucket.arn}/*"
       }
     ]
   })
@@ -43,8 +44,9 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   depends_on = [aws_s3_bucket_public_access_block.bucket_public_block]
 }
 
+# Configurar el Bucket como un sitio web estático
 resource "aws_s3_bucket_website_configuration" "s3_pagina" {
-  bucket = aws_s3_bucket.s3.id
+  bucket = aws_s3_bucket.mi_bucket.id
 
   index_document {
     suffix = "index.html"
